c589d3cf51e3454c51981e25d2f13db5b4961126
pairrz <dararat.buayim@gmail.com>
2023-12-12 14:51:24 +0700

    02

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,34 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char rank;
+  cout << "Input your rank: ";
+  cin >> rank;
+    if(rank == 'S'){
+        cout << "You have received Super Ultra Rare Unit!!!\n";
+        cout << "You have received 5 gems.\n";
+        cout << "You have received 1 gems.\n";
+        cout << "You have received 2000 coins.\n";
+        cout << "You have received very KAK items.\n";
+        }
+    else if(rank == 'A'){
+        cout << "You have received 5 gems.\n";
+        cout << "You have received 1 gems.\n";
+        cout << "You have received 2000 coins.\n";
+        cout << "You have received very KAK items.\n";
+        }
+    else if(rank == 'B'){
+        cout << "You have received 1 gems.\n";
+        cout << "You have received 2000 coins.\n";
+        cout << "You have received very KAK items.\n";
+        }
+    else if(rank == 'C'){
+        cout << "You have received 2000 coins.\n";
+        cout << "You have received very KAK items.\n";
+        }
+    else if(rank == 'D') cout << "You have received very KAK items.\n";
+    
+  return 0;
+}
